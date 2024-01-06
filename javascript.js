b861768441873eacd1d18d95ecf43a408130f5e4
manjeet <manjeetchoudhary97832@gmail.com>
2024-01-06 16:39:15 +0530

    add code

--- javascript.js
+++ javascript.js
@@ -5,3 +5,59 @@ console.log(x,y)
 let a=2+1;
 let b=3;
 console.log(a==b)// output --- true
+
+
+let arr=[1,5,7,4,2,3,6,4,1,2,5,4,1,2,9,8,7,4,1,3,5,2,1,4,6]
+let kk={}
+arr.map((n)=>{
+    if(kk[n]){
+        kk[n]++
+    }else{
+        kk[n]=1
+    }
+})
+console.log(kk)
+// output --{
+//   '1': 5,
+//   '2': 4,
+//   '3': 2,
+//   '4': 5,
+//   '5': 3,
+//   '6': 2,
+//   '7': 2,
+//   '8': 1,
+//   '9': 1
+// }
+
+let uniqueArray = arr.filter((value, index, self) => {
+  return self.indexOf(value) === index;
+});
+console.log(uniqueArray);
+// output ---[
+//     1, 5, 7, 4, 2,
+//     3, 6, 9, 8
+//   ]
+
+let uniqueArrayy = arr.reduce((accumulator, currentValue) => {
+  if (!accumulator.includes(currentValue)) {
+    accumulator.push(currentValue);
+  }
+  return accumulator;
+}, []);
+console.log(uniqueArrayy);
+// output ---[
+//     1, 5, 7, 4, 2,
+//     3, 6, 9, 8
+//   ]
+
+let dd=arr.reduce(function(acc,current){
+   if(!acc.includes(current)){
+    acc.push(current)
+   }
+   return acc;
+}, []);
+console.log('dd',dd)
+// output ---[
+//     1, 5, 7, 4, 2,
+//     3, 6, 9, 8
+//   ]

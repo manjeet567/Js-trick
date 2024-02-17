61e66ca4a4abee84e0158e3ad6bb6b065f08620d
manjeet <manjeetchoudhary97832@gmail.com>
2024-02-17 12:43:00 +0530

    dsf

--- primeNumber.js
+++ primeNumber.js
@@ -4,10 +4,10 @@ function primeNumber(num){
     }else{
         for(let i=2;i<=Math.sqrt(num);i++){
             if(num % i== 0){
-                return console.log(`${num} is a not a prime Number`);
+                return console.log(`${num} is not a prime Number`);
             }
         }
         return console.log(`${num} is a prime Number`);
     }
 }
-primeNumber(2)
+primeNumber(4)
